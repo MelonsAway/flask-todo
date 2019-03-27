39506908d07a208e218d953e7c9cea7a3cddf240
Jason Wyatt <stryfecrab22@gmail.com>
2019-03-27 16:41:13 -0400

    first commit

---
+++ flask_todo/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    @app.route('/hello')
+    def hello():
+        name = request.args.get('name', 'world')
+#        return f"Hello {name}!"
+        return render_template('hello.html', name=name)
+    return app
